1627c7c28e7dc5e39f44c3d41faa1c51ce5383fe
evasoldis <evasoldis13@gmail.com>
2026-04-07 22:44:15 +0000

    commit
    stylesheet n java edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,4 +23,101 @@ docReady(function() {
 
 });
 
+// images that animate on scroll
+const mapping = [
+  { section: "intro", image: "img-intro" },
+  { section: "backg", image: "img-backg" },
+  { section: "chap1", image: "img-chap1" },
+  { section: "chap2", image: "img-chap2" },
+  { section: "chap3", image: "img-chap3" },
+  { section: "conclusion", image: "img-conclusion" },
+];
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const match = mapping.find(m => m.section === entry.target.id);
+
+    if (!match) return;
+
+    const img = document.getElementById(match.image);
+
+    if (entry.isIntersecting) {
+      // scrolling into section → show image
+      img.classList.add("active");
+    } else {
+      // scrolling out of section → hide image
+      img.classList.remove("active");
+    }
+  });
+}, {
+  root: null,
+  rootMargin: "-30% 0px -30%",
+  threshold: 0
+});
+
+// button for sidenotes
+mapping.forEach(m => {
+  const el = document.getElementById(m.section);
+  if (el) observer.observe(el);
+});
+
+const btn = document.getElementById("toggleBtn");
+const sidenote = document.getElementById("sidenote");
+
+btn.addEventListener("click", () => {
+  sidenote.classList.toggle("hidden");
+
+  // Optional: change button text
+  if (sidenote.classList.contains("hidden")) {
+    btn.textContent = "*";
+  } else {
+    btn.textContent = "*";
+  }
+});
+
+btn.addEventListener("click", () => {
+  btn.classList.add("clicked");
+});
+
+
+
+
+
+// chapter navigation
+// function goToChapter(id) {
+//   document.getElementById(id).scrollIntoView({
+//     behavior: "smooth"
+//   });
+// }
+
+
+
+let chapterList = [ ];
+
+const chapterLinks = document.querySelectorAll( 'nav ul a[href^="#"]' );
+chapterLinks.forEach( link => chapterList.push(document.querySelector( link.getAttribute('href') ) ) );
+
+chapterList.reverse( );
+
+
+window.addEventListener( 'scroll', ( ) => {
+  const scrollTop = window.pageYOffset;
+  let matchFound = false;
+
+  chapterList.forEach( ( chapter, index ) => {
+    const heading = chapter.querySelector( ':scope h2' );
+    const headingStyle = window.getComputedStyle( heading );
+    const headingMarginTop = parseFloat( headingStyle.getPropertyValue( 'margin-top' ) );
+    const headingTop = chapter.offsetTop - headingMarginTop;
+
+    const chapterLink = chapterLinks[ chapterLinks.length - index - 1 ];
+
+    if ( !matchFound && headingTop - scrollTop < 50 ) {
+      chapterLink.classList.add( 'active' );
+
+      matchFound = true;
+    } else { 
+      chapterLink.classList.remove( 'active' );
+    }
+  } );
+} );
